1de3263a9a90fb173ef21d02589637adfdcbf67f
Unknown <luigiaiello97@gmail.com>
2018-11-07 11:00:34 +0100

    First commit

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Post } from '../post.model';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+
+export class PostCreateComponent implements OnInit {
+  enteredTitle = '';
+  enteredContent = '';
+  post: Post;
+  isLoading = false;
+  private mode = 'create';
+  private postId: string;
+
+  // postCreated = new EventEmitter<Post>();
+
+  // onAddPost(postInput: HTMLTextAreaElement) {
+  //   this.newPost = postInput.value;
+  // }
+  // onAddPost() {
+  //   const post: Post = {
+  //     title: this.enteredTitle,
+  //     content: this.enteredContent
+  //   };
+  //   this.postCreated.emit(post);
+  // }
+
+  constructor(public postsService: PostsService, public route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        // Start fetching
+        this.isLoading = true;
+        this.postsService.getPost(this.postId)
+          .subscribe(postData => {
+            // End fetching
+            this.isLoading = false;
+            this.post = {id: postData._id, title: postData.title, content: postData.content};
+          });
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  }
+
+  onSavePost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    this.isLoading = true;
+    if (this.mode === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+    }
+
+    form.resetForm();
+  }
+}
---
+++ src/app/posts/posts.service.ts
@@ -0,0 +1,85 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Post } from './post.model';
+import { Router } from '@angular/router';
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+  private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getPosts() {
+    // return [...this.posts]; // Create a true copy of the posts (typescript). I create a new array with old objects
+    this.http
+      .get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
+  getPost(id: string) {
+    return this.http.get<{_id: string, title: string, content: string}>('http://localhost:3000/api/posts/' + id);
+  }
+
+  addPost(title: string, content: string) {
+    const post: Post = { id: null, title: title, content: content };
+    console.log(post);
+    this.http
+      .post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
+      .subscribe((responseData) => {
+        const postId = responseData.postId;
+        post.id = postId; // Javascript has a reference type so you can access safetly to an attribute of the object
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+        this.router.navigate(['/']);
+      });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = {
+      id: id,
+      title: title,
+      content: content
+    };
+    this.http
+    .put('http://localhost:3000/api/posts/' + id, post)
+    .subscribe(response => {
+      const updatedPosts = [...this.posts];
+      const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+      updatedPosts[oldPostIndex] = post;
+      this.posts = updatedPosts;
+      this.postsUpdated.next([...this.posts]);
+      this.router.navigate(['/']);
+    });
+  }
+
+  deletePost(postId: String) {
+    this.http.delete('http://localhost:3000/api/posts/' + postId)
+      .subscribe(() => {
+        const updatedPosts = this.posts.filter(post => {
+          return post.id !== postId;
+        });
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+}
